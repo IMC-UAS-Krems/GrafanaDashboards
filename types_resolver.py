14c24965a6b03d2f937de02a3aed658329f089de
bobokrut <egor-evlampiev@mail.ru>
2023-10-19 15:59:48 +0200

    add module to resolve types of a data field for Grafana

---
+++ types_resolver.py
@@ -0,0 +1,267 @@
+from typing import Literal, TypeAlias
+
+from yaml import load
+
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+
+import requests
+
+Type: TypeAlias = Literal["Number", "String", "Array"]
+
+
+class Keys(list):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class TypesResolver:
+    """
+    This class is used to resolve the type of a given path in a given type or data source
+
+    >>> with TypesResolver() as tr:
+    >>>     tr.resolve("AirQualityObserved", "stationName", "url")
+    """
+
+    def __init__(self) -> None:
+        self._spec_cache = {}
+        self._data_cache = {}
+        self._urls = {
+            "AirQualityObserved": "https://raw.githubusercontent.com/smart-data-models/dataModel.Environment/38ab75f0c13b2a838a45241132165cbefe448ca6/AirQualityObserved/model.yaml",
+            "PointOfInterest": "https://raw.githubusercontent.com/smart-data-models/dataModel.PointOfInterest/d093271220bdd173d31b3388f0b00953ebbdc0cd/PointOfInterest/model.yaml",
+        }
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.clear_cache()
+
+    def clear_cache(self):
+        self._spec_cache = {}
+        self._data_cache = {}
+
+    def resolve(self, type: str, path: str, data_source_url: str) -> Keys | Type | None:
+        """
+        Tries to resolve the type of a given path in a given type or data source.
+
+        Returns `None` if the path could not be resolved, else any of the following:
+        - Keys(list): If the path is a key of a dictionary
+        - Type: If the path is a value of a dictionary. Can be "Number", "String", "Array" or "Datetime
+
+        Args:
+            - type: The type of the data source
+            - path: The path to resolve
+            - data_source_url: The url of the data source
+
+        Returns:
+        The type of the path
+
+
+        """
+        if result := self._resolve_specs(type, path):
+            return result
+
+        if result := self._resolve_data(data_source_url, path):
+            return result
+
+        return None
+
+    def _resolve_specs(self, type: str, path: str) -> Keys | Type | None:
+        specs = self._get_specs(type)
+        to_return = None
+
+        for p in path.split("."):
+            if result := self._parse_standart(p):
+                return result
+
+            specs, to_return = self._resolve_type_from_specs(specs, p)
+
+            if to_return is None:
+                return None
+
+        return to_return
+
+    def _resolve_data(self, url: str, path: str) -> Keys | Type | None:
+        data = self._get_data(url)
+        to_return = None
+
+        for p in path.split("."):
+            data, to_return = self._resolve_type_from_data(data, p)
+
+            if to_return is None:
+                return None
+
+        return to_return
+
+    def _get_specs(self, type: str) -> dict:
+        if not self._spec_cache.get(type):
+            r = requests.get(self._urls[type])
+            specs = load(r.text, Loader=Loader)
+            self._spec_cache[type] = specs[type]["properties"]
+        return self._spec_cache[type]
+
+    def _get_data(self, url: str) -> dict:
+        if not self._data_cache.get(url):
+            r = requests.get(url).json()[0]
+            self._data_cache[url] = r
+        return self._data_cache[url]
+
+    def _resolve_type_from_specs(
+        self, specs: dict, path: str
+    ) -> tuple[dict, Type | None | Keys]:
+        specs = specs.get(path) or specs.get(path.lower().replace(".", ""))
+
+        if not specs:
+            return specs, None
+
+        if specs["type"] == "object":
+            to_return = Keys(specs["properties"].keys())
+            specs = specs["properties"]
+            return specs, to_return
+
+        to_return = specs["type"].capitalize()
+        return specs, to_return
+
+    def _resolve_type_from_data(
+        self, data: dict, path: str
+    ) -> tuple[dict, Type | Keys | None]:
+        value = data.get(path)
+
+        if not value:
+            return data, None
+
+        type = value.get("type") if isinstance(value, dict) else None
+
+        if type == "Number":
+            return data, "Number"
+
+        elif type == "Text":
+            return data, "String"
+
+        elif type == "DateTime":
+            return data, "String"
+
+        elif type == "List":
+            return data, "Array"
+
+        elif type == "URL":
+            return data, "String"
+
+        elif type == "geo:json":
+            return data, "Array"
+
+        elif type is None:
+            if isinstance(value, dict):
+                return value, Keys(value.keys())
+            if isinstance(value, list):
+                return value[0], "Array"
+            if isinstance(value, str):
+                return data, "String"
+            if isinstance(value, int):
+                return data, "Number"
+            return data, None
+        else:
+            value = value["value"]
+
+            if isinstance(value, dict):
+                return value, Keys(value.keys())
+
+            if isinstance(value, list):
+                return value[0], "Array"
+
+            return data, None
+
+    def _parse_standart(self, path: str) -> Type | None:
+        if path.lower() == "location":
+            return "Array"
+
+
+if __name__ == "__main__":
+    with TypesResolver() as tr:
+        print(
+            "stationName: {}".format(
+                tr.resolve(
+                    "AirQualityObserved",
+                    "stationName",
+                    data_source_url="https://data.iiss.at/dataskop/fiwarenosec/v2/entities?type=AirQualityObserved",
+                )
+            )
+        )
+        print(
+            "location: {}".format(
+                tr.resolve(
+                    "AirQualityObserved",
+                    "location",
+                    data_source_url="https://data.iiss.at/dataskop/fiwarenosec/v2/entities?type=AirQualityObserved",
+                )
+            )
+        )
+        print(
+            "address: {}".format(
+                tr.resolve(
+                    "AirQualityObserved",
+                    "address",
+                    data_source_url="https://data.iiss.at/dataskop/fiwarenosec/v2/entities?type=AirQualityObserved",
+                )
+            )
+        )
+        print(
+            "address.addressCountry: {}".format(
+                tr.resolve(
+                    "AirQualityObserved",
+                    "address.addressCountry",
+                    data_source_url="https://data.iiss.at/dataskop/fiwarenosec/v2/entities?type=AirQualityObserved",
+                )
+            )
+        )
+        print(
+            "NO2: {}".format(
+                tr.resolve(
+                    "AirQualityObserved",
+                    "NO2",
+                    data_source_url="https://data.iiss.at/dataskop/fiwarenosec/v2/entities?type=AirQualityObserved",
+                )
+            )
+        )
+        print(
+            "NOx: {}".format(
+                tr.resolve(
+                    "AirQualityObserved",
+                    "NOx",
+                    data_source_url="https://data.iiss.at/dataskop/fiwarenosec/v2/entities?type=AirQualityObserved",
+                )
+            )
+        )
+
+        print(
+            "validity: {}".format(
+                tr.resolve(
+                    "AirQualityObserved",
+                    "validity",
+                    data_source_url="https://data.iiss.at/dataskop/fiwarenosec/v2/entities?type=AirQualityObserved",
+                )
+            )
+        )
+
+        print(
+            "validity.to: {}".format(
+                tr.resolve(
+                    "AirQualityObserved",
+                    "validity.to",
+                    data_source_url="https://data.iiss.at/dataskop/fiwarenosec/v2/entities?type=AirQualityObserved",
+                )
+            )
+        )
+
+        print(
+            "dateObserved: {}".format(
+                tr.resolve(
+                    "AirQualityObserved",
+                    "dateObserved",
+                    data_source_url="https://data.iiss.at/dataskop/fiwarenosec/v2/entities?type=AirQualityObserved",
+                )
+            )
+        )
