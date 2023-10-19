12ce9df100f9a727b1072b4d952421925396d313
bobokrut <egor-evlampiev@mail.ru>
2023-10-19 23:57:00 +0200

    add: an example how to use jinja2

---
+++ grafana_dashboards/main.py
@@ -0,0 +1,19 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import json
+
+
+env = Environment(
+    loader=FileSystemLoader(searchpath='templates'),
+    autoescape=select_autoescape("json")
+)
+env.filters['jsonify'] = json.dumps
+
+pie = env.get_template('pie_chart.json')
+scatter = env.get_template('scatter.json')
+config = env.get_template('config.json')
+
+pie = json.loads(pie.render(parts=["one", "two", "three"], title="Pie Chart"))
+scatter = json.loads(scatter.render(x="one", y=["two", "three"], title="Scatter Chart"))
+config = json.loads(config.render(config={"plots":[pie, scatter]}))
+
+print(json.dumps(config, indent=4))
