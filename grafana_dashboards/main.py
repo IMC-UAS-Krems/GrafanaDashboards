f5ca09e96f093c205635e96b0941feccb98f1e71
bobokrut <egor-evlampiev@mail.ru>
2023-10-20 20:45:57 +0200

    fix: now uvicorn starts correctly

--- grafana_dashboards/main.py
+++ grafana_dashboards/main.py
@@ -1,13 +1,16 @@
+import logging
 from typing import TypeAlias
 
 import uvicorn
 from fastapi import FastAPI
 
-from .model import Config
+from model import Config
+import logging_setup # will be executed on import
 
 GrafanaModel: TypeAlias = dict
 
 app = FastAPI()
+logger = logging.getLogger("grafana_dashboards")
 
 
 @app.get("/")
@@ -16,4 +19,4 @@ async def root(config: Config) -> GrafanaModel:
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0:8000", reload=True)
+    uvicorn.run("main:app", port=8000, reload=True)
