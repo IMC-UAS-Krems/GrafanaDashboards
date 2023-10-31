7430b44497c12df12df40947d8f53610778519f3
bobokrut <egor-evlampiev@mail.ru>
2023-10-31 18:37:32 +0100

    fix: types are now written in lowercase + use type to query the correct data

--- grafana_dashboards/types_resolver.py
+++ grafana_dashboards/types_resolver.py
@@ -10,7 +10,7 @@ except ImportError:
 
 import requests
 
-Type: TypeAlias = Literal["Number", "String", "Time", "Boolean"]
+Type: TypeAlias = Literal["number", "string", "time", "boolean"]
 
 
 class Keys(list):
@@ -18,10 +18,10 @@ class Keys(list):
 
 
 class List(Enum):
-    Number = "Number"
-    String = "String"
-    Time = "Time"
-    Boolean = "Boolean"
+    Number = "number"
+    String = "string"
+    Time = "time"
+    Boolean = "boolean"
 
 
 class TypesResolver:
@@ -86,7 +86,7 @@ class TypesResolver:
         if result := self._resolve_specs(type, path):
             return result
 
-        if result := self._resolve_data(data_source_url, path):
+        if result := self._resolve_data(data_source_url, path, type):
             return result
 
         return None
@@ -108,10 +108,12 @@ class TypesResolver:
 
         return to_return
 
-    def _resolve_data(self, url: str, path: str) -> List | Keys | Type | None:
+    def _resolve_data(
+        self, url: str, path: str, type: str
+    ) -> List | Keys | Type | None:
         """"""
 
-        data = self._get_data(url)
+        data = self._get_data(url, type)
         to_return = None
 
         for p in path.split("."):
@@ -132,11 +134,11 @@ class TypesResolver:
 
         return self._spec_cache[type]
 
-    def _get_data(self, url: str) -> dict:
+    def _get_data(self, url: str, type: str) -> dict:
         """"""
 
         if not self._data_cache.get(url):
-            r = requests.get(url).json()[0]
+            r = requests.get(url, params={"type": type}).json()[0]
             self._data_cache[url] = r
 
         return self._data_cache[url]
@@ -167,12 +169,12 @@ class TypesResolver:
 
         if specs["type"] == "string":
             if specs.get("format") == "date-time":
-                return specs, "Time"
+                return specs, "time"
 
-            return specs, "String"
+            return specs, "string"
 
         if specs["type"] == "number":
-            return specs, "Number"
+            return specs, "number"
 
         return specs, None
 
@@ -189,13 +191,13 @@ class TypesResolver:
         type = value.get("type") if isinstance(value, dict) else None
 
         if type == "Number":
-            return data, "Number"
+            return data, "number"
 
         elif type == "Text":
-            return data, "String"
+            return data, "string"
 
         elif type == "DateTime":
-            return data, "Time"
+            return data, "time"
 
         elif type == "List":
             value = value["value"]
@@ -209,7 +211,7 @@ class TypesResolver:
             return data, None
 
         elif type == "URL":
-            return data, "String"
+            return data, "string"
 
         elif type == "geo:json":
             return data, List.Number
@@ -228,10 +230,10 @@ class TypesResolver:
                 return data, None
 
             if isinstance(value, str):
-                return data, "String"
+                return data, "string"
 
             if isinstance(value, int):
-                return data, "Number"
+                return data, "number"
 
             return data, None
 
